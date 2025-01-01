aff465dc1d043a09774533fdd462a2aa695b6734
Larissa-alSouza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-01-01 13:14:23 -0300

    Create exercicio1.cpp

---
+++ Introdução à linguagem C/exercicio1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//1- Faça um programa que leia um número inteiro e o imprima
+int main() {
+    
+    int num = 0;
+    
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &num);
+    
+    printf("O numero inteiro inserido foi: %d", num);
+
+    return 0;
+}
