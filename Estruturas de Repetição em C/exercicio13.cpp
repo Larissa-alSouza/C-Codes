5c2c810ac7058f2da53fa4b51a2f190d0f66f7ce
Larissa-alSouza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-01-14 22:04:24 -0300

    Create exercicio13.cpp

---
+++ Estruturas de Repetição em C/exercicio13.cpp
@@ -0,0 +1,12 @@
+/* 13- Faça um programa que imprima de 1 a 10 utilizando for */
+
+#include <stdio.h>
+
+int main() {
+    int i;
+  
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
