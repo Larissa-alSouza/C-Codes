6db643009837541e8004efe2490455527a6fdd6b
Larissa-alSouza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-01-16 21:41:05 -0300

    Create exercicio17_1.cpp

---
+++ Estruturas de Repetição em C/exercicio17_1.cpp
@@ -0,0 +1,16 @@
+/* 17.1- Use um loop for ou while para imprimir os números de 1 a 20, um por linha. */
+
+#include <stdio.h>
+
+int main() {
+    
+    int num = 0;
+    
+    while(num < 21) {
+        printf("%d \n", num);
+        
+        num++;
+    }
+
+    return 0;
+}
