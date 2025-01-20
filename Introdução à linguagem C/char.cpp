b3d32b6813f129de0c4f52348c2ab070d4d4f99b
Larissa Alves de Souza <162065841+Larissa-alSouza@users.noreply.github.com>
2025-01-20 15:39:37 -0300

    Create char.cpp

---
+++ Introdução à linguagem C/char.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+    char palavra[20];
+    
+    printf("Digite uma palavra: ");
+    scanf("%s", &palavra);
+    
+    printf("A palavra é: %s", palavra);
+
+    return 0;
+}
